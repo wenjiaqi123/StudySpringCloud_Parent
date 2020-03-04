26b99e7ef5d83feb50e060041673db7ff33a2602
wjq <939949243@qq.com>
2020-03-05 03:48:44 +0800

    lombok

---
+++ StudySpringCloud_Common/src/main/java/com/gsm/StudyspringcloudCommonApplication.java
@@ -0,0 +1,13 @@
+package com.gsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyspringcloudCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyspringcloudCommonApplication.class, args);
+    }
+
+}
---
+++ StudySpringCloud_Common/src/test/java/com/gsm/studyspringcloud_common/StudyspringcloudCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsm.studyspringcloud_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyspringcloudCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
