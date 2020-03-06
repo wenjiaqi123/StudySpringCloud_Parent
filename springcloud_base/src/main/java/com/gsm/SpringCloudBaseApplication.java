59260fe380e5634a0515a10035a2d3ea8b233c8d
wjq <939949243@qq.com>
2020-03-06 08:00:54 +0800

    redis

---
+++ springcloud_base/src/main/java/com/gsm/SpringCloudBaseApplication.java
@@ -0,0 +1,14 @@
+package com.gsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//  @SpringBootApplication  SpringBoot启动注解
+@SpringBootApplication
+public class SpringCloudBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudBaseApplication.class, args);
+    }
+}
+
